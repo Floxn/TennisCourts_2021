8c58cb8fee0d7bc3e008708c7bfed748beaa5f8d
Florian Krohmer <f.krohmer@seidemann-web.com>
2021-09-22 16:26:55 +0200

    add more functionality

--- Tennis-Court-Test/js/scripts.js
+++ Tennis-Court-Test/js/scripts.js
@@ -1,4 +1,5 @@
 let tennisFacility = [];
+var players = {};
 
 // Tennis facility has more than one tennis court
 // tennis facility {
@@ -7,30 +8,15 @@ let tennisFacility = [];
 //  tennis court
 // }
 
-// Object to build a Player
-function Player(forename, lastname) {
-    this.forename = forename;
-    this.lastname = lastname;
-}
-
-// Object to build a Tennis Court
-function Court(courtNumber, player1, player2, player3, player4) {
-    this.courtNumber = courtNumber;
-    this.player1 = player1;
-    this.player2 = player2;
-    this.player3 = player3;
-    this.player4 = player4;
-}
-
-
+// add a new Court to the tennis facility
 function addNewCourt() {
-    var newCourt = new Court(tennisFacility.length + 1, "Florian Krohmer", "Zina Krohmer", "Hans Krohmer", "Nochein Krohmer");
-    tennisFacility.push(newCourt);
-    console.log("Court no. " + newCourt.courtNumber + " is added");
-    console.log(newCourt.player1 + newCourt.player2 + newCourt.player3 + newCourt.player4);
+    addNewPlayers();
+    addPlayersToCourt();
+
     console.log(tennisFacility);
 }
 
+// remove last court from tennis facility
 function removeLastCourt() {
     if (tennisFacility.length > 0) {
         console.log("Court no. " + tennisFacility.length + " is deleted");
@@ -41,7 +27,52 @@ function removeLastCourt() {
     }
 }
 
-let trigger = document.getElementById("header");
-trigger.addEventListener("click", machWas);
+
+//// BUILD OBJECTS
+////
+// Object to build a Player
+function Player(forename, lastname) {
+    this.forename = forename;
+    this.lastname = lastname;
+}
+
+// Object to build a Tennis Court
+// On one court it is possible to enter 4 player
+function Court(courtNumber) {
+
+    this.courtNumber = courtNumber;
+}
+
+// add players to players array
+function addNewPlayers() {
+    let numberOfPlayer = howManyPlayers();
+
+    // iterate through the Number who is set via prompt
+    for (var i = 0; i < numberOfPlayer; i++) {
+        let playerForename = prompt("Please enter Forename");
+        let playerLastname = prompt("Please enter Lastname");
+        players[i] = new Player(playerForename, playerLastname);
+    }
+}
+
+function howManyPlayers() {
+    // need input to get number of player
+    let numberOfPlayer = (function ask() {
+        var n = prompt('How many Players would join the game? 1 to 4:');
+        return isNaN(n) || +n > 4 || +n < 1 ? ask() : n;
+    }());
+    return numberOfPlayer;
+}
+
+function addPlayersToCourt() {
+    let newCourt = new Court(tennisFacility.length + 1);
+
+    newCourt.players = players;
+
+    tennisFacility.push(newCourt);
+    console.log("Court no. " + newCourt.courtNumber + " is added");
+    return players = {};
+}
+
 document.getElementById("addCourt").addEventListener("click", addNewCourt);
 document.getElementById("removeCourt").addEventListener("click", removeLastCourt);
