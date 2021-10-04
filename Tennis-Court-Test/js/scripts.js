abc203a1955cc0bcf955fe98f9a6518a6b0a3d72
Florian Krohmer <f.krohmer@seidemann-web.com>
2021-10-04 10:37:16 +0200

    add css and update script

--- Tennis-Court-Test/js/scripts.js
+++ Tennis-Court-Test/js/scripts.js
@@ -1,34 +1,10 @@
+
+// TODO next: Make Timetable for each Court to add players on court for each Hour for example 17-18 o´clock order 09-10 o´clock
+
 let tennisFacility = [];
 var players = {};
 
-// Tennis facility has more than one tennis court
-// tennis facility {
-//  tennis court
-//  tennis court
-//  tennis court
-// }
-
-// add a new Court to the tennis facility
-function addNewCourt() {
-    addNewPlayers();
-    addPlayersToCourt();
-
-    console.log(tennisFacility);
-}
-
-// remove last court from tennis facility
-function removeLastCourt() {
-    const courtCounter = tennisFacility.length
-    if (courtCounter > 0) {
-        document.getElementById('court-' + courtCounter).remove();
-        console.log("Court no. " + courtCounter + " is deleted");
-        tennisFacility.pop();
-        console.log(tennisFacility);
-    } else {
-        console.log("There is no Court to delete");
-    }
-}
-
+const facility = document.getElementById("tennisFacility");
 
 //// BUILD OBJECTS
 ////
@@ -41,69 +17,131 @@ function Player(forename, lastname) {
 // Object to build a Tennis Court
 // On one court it is possible to enter 4 player
 function Court(courtNumber) {
-
     this.courtNumber = courtNumber;
+    this.players = {};
 }
 
-// add players to players array
-function addNewPlayers() {
-    let numberOfPlayer = howManyPlayers();
-
-    // iterate through the Number who is set via prompt
-    for (var i = 0; i < numberOfPlayer; i++) {
-        let playerForename = prompt("Please enter Forename");
-        let playerLastname = prompt("Please enter Lastname");
-        players[i] = new Player(playerForename, playerLastname);
-    }
-}
-
-function howManyPlayers() {
-    // need prompt to get number of player
-    // max number of player is 4
-    let numberOfPlayer = (function ask() {
-        var n = prompt('How many Players would join the game? 1 to 4:');
-        return isNaN(n) || +n > 4 || +n < 1 ? ask() : n;
+function howManyCourts() {
+    let numberOfCourts = (function ask() {
+        var number = prompt('How many Courts are on the tennis facility?');
+        return isNaN(number) || +number <= 0 ? ask() : number;
     }());
-    return numberOfPlayer;
+    addNewCourt(numberOfCourts);
+}
+
+function addNewCourt(numberOfCourts) {
+    console.log('Number of Courts:' + numberOfCourts)
+    for (let courtNumber = 1; courtNumber <= numberOfCourts; courtNumber++) {
+
+        buildCourtHTML(courtNumber);
+
+        let newCourt = new Court(tennisFacility.length + 1);
+        tennisFacility.push(newCourt);
+    }
+    addPlayerButtonEventListener();
+
+}
+
+function buildCourtHTML(courtNumber) {
+    const courtNumberClass = 'court-' + courtNumber;
+
+    // Build Court Wrapper
+    newCourt = document.createElement('div');
+    newCourt.classList.add('court', courtNumberClass);
+    newCourt.id = courtNumberClass;
+    const courtElement = newCourt;
+
+    // Build Court Headline
+    courtHeadline = document.createElement('h2');
+    courtHeadline.textContent = 'Court-No: ' + courtNumber;
+
+    // Build Player Wrapper
+    courtPlayers = document.createElement('div');
+    courtPlayers.classList.add('court-players');
+
+    // Build add Player Button
+    addPlayerButton = document.createElement('button');
+    addPlayerButton.classList.add('add-player', 'add-player-' +courtNumberClass);
+    addPlayerButton.textContent = 'add new Player';
+
+    facility.appendChild(courtElement);
+    courtElement.appendChild(courtHeadline);
+    courtElement.appendChild(courtPlayers);
+    courtElement.appendChild(addPlayerButton);
+}
+
+function buildPlayerHTML(playerForename, playerLastname, playerNumber) {
+    // build new players element and append it
+    newPlayerElement = document.createElement('div');
+    newPlayerElement.classList.add('player')
+    newPlayerElement.setAttribute('data-player', playerNumber);
+    newPlayerElement.textContent = playerForename + ' ' + playerLastname;
+
+    removeButton = document.createElement('button');
+    removeButton.classList.add('remove-player');
+    removeButton.textContent = '-';
+    newPlayerElement.appendChild(removeButton);
 }
 
 function addPlayersToCourt() {
-    // set a new Court
-    let newCourt = new Court(tennisFacility.length + 1);
 
-    // write all players as a new Property
-    newCourt.players = players;
+    // get the court id
+    let courtId = this.parentElement.id;
+    courtId = courtId.replace('court-','');
+    courtId = parseInt(courtId);
 
-    // add the new court to the facility
-    tennisFacility.push(newCourt);
-    console.log("Court no. " + newCourt.courtNumber + " is added");
+    // get the name of the player who will be added
+    const playerForename = prompt("Please enter Forename");
+    const playerLastname = prompt("Please enter Lastname");
+    const newPlayer = new Player(playerForename, playerLastname);
 
-    buildHtmlAddCourtWithPlayer(newCourt.courtNumber);
-    return players = {};
-}
+    // find the correct Court object
+    let currentCourt = tennisFacility.find(Court => Court.courtNumber === courtId);
 
-function buildHtmlAddCourtWithPlayer(courtCounter) {
-    const facility = document.getElementById("tennisFacility");
-    const courtNumberClass = 'court-' + courtCounter;
-    let playerNumber = 0;
+    // get length of Players Object in the current court
+    let courtPlayerLength = Object.keys(currentCourt.players).length + 1;
+    playerNumber = 'player-' + courtPlayerLength;
+    currentCourt.players[playerNumber] = newPlayer;
 
-    // add tennis court html to tennis facility block
-    facility.innerHTML += '<div class="tennisCourt ' + courtNumberClass + '" id="' + courtNumberClass + '"><h2>Court-No: ' + courtCounter + '</h2></div>';
+    // add Player
+    buildPlayerHTML(playerForename, playerLastname, playerNumber);
+    this.previousElementSibling.appendChild(newPlayerElement);
 
-    const court = document.getElementById(courtNumberClass);
-
-    // add every single player to the court
-    Object.entries(players).forEach(([key, player], index) => {
-        playerNumber++;
-
-        playerName = player.forename + ' ' + player.lastname;
-        newPlayer = '<div class="tennisPlayer player-' + playerNumber + '">' + playerName + '</div>';
-
-        court.innerHTML += newPlayer;
-
-    })
+    // trigger eventListener
+    removePlayerButtonEventListener();
+    console.log(tennisFacility);
 
 }
 
-document.getElementById("addCourt").addEventListener("click", addNewCourt);
-document.getElementById("removeCourt").addEventListener("click", removeLastCourt);
+function removePlayerFromCourt() {
+    const parent = this.parentNode;
+    const currentPlayersId = parent.getAttribute('data-player');
+    let playersCourtId = parent.parentNode.parentNode.id;
+    playersCourtId = playersCourtId.replace('court-','');
+    playersCourtId = parseInt(playersCourtId);
+
+    let currentCourt = tennisFacility.find(Court => Court.courtNumber === playersCourtId);
+    delete currentCourt.players[currentPlayersId];
+
+    parent.parentNode.removeChild(this.parentNode);
+    console.log('Player ' + currentPlayersId + ' is deleted from Court-id: ' + playersCourtId);
+
+}
+
+function addPlayerButtonEventListener() {
+    let addPlayerButtons = document.querySelectorAll('.add-player');
+
+    for (let i = 0; i < addPlayerButtons.length; i++) {
+        addPlayerButtons[i].addEventListener('click', addPlayersToCourt);
+    }
+}
+
+function removePlayerButtonEventListener() {
+    let removePlayerButtons = document.querySelectorAll('.remove-player');
+
+    for (let i = 0; i < removePlayerButtons.length; i++) {
+        removePlayerButtons[i].addEventListener('click', removePlayerFromCourt);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", howManyCourts);
