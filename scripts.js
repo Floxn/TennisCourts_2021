0b8131156d12e58f21bee1440472840803e6d4b2
Floxn <florian.krohmer@gmail.com>
2023-02-10 21:30:55 +0100

    reorder files

--- scripts.js
+++ scripts.js
@@ -1,10 +1,3 @@
-// TODO next: Check if player id is already exists in court and time slot
-// TODO next: rewrite to add new player int players
-// TODO next: Weekly View
-// TODO next: Move every Day of the Week in one Tab
-// TODO next: change eventhandler from click or touch to pointerEvents
-// TODO next next :) : Include Database
-
 const courtNumberInput = document.querySelector('#howManyCourts');
 
 function startPlaning() {
