b5c3cd24575c70412c41be14e661fbf680365e78
Floxn <florian.krohmer@gmail.com>
2023-03-12 22:45:05 +0100

    add loop to get more timeslots

--- modular/facility.js
+++ modular/facility.js
@@ -26,14 +26,15 @@ const player1 = new Player()
 player1.firstname = "Franz";
 player1.surname = "Müller";
 
-const timeslot1 = new Timeslot();
-timeslot1.begin = '7:00 Uhr';
-timeslot1.end = `8:00 Uhr`;
-timeslot1.player = player1;
-console.log(timeslot1.player)
+for (let timeSlots = 7; timeSlots <= 21; timeSlots++) {
+    const timeslot1 = new Timeslot();
+    timeslot1.begin = `${timeSlots}:00 Uhr`;
+    timeslot1.end = `${timeSlots + 1}:00 Uhr`;
+    timeslot1.player = player1;
 
-document.body.appendChild(timeslot1.render())
-
+    console.log(timeslot1.player)
+    document.body.appendChild(timeslot1.render())
+}
 
 for (let i = 0; i < 5; i++) {
     const court = new Court();
